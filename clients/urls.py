407f398ad8b503d198ff31b4b5bfca239879f6f0
honorezemagho <honorezemagho@gmail.com>
2021-07-15 05:30:02 +0100

    layout templating and show clients details

--- clients/urls.py
+++ clients/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views 
 
 urlpatterns = [
-    path('clients', views.index)
+    path('clients', views.index, name='clients'),
+    path('clients/<slug:client_slug>', views.show_clients, name='client-details')
 ]
--- clients/views.py
+++ clients/views.py
@@ -4,7 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     clients = [
-        {'name': 'Mika Albry'},
-        {'name': 'Tamka Alfred'}
+        {'name': 'Mika Albry', 'phone': '+33 11457896','slug': 'client-1'},
+        {'name': 'Tamka Alfred', 'phone': '+237 654789542', 'slug': 'client-2'}
     ];
     return render(request,'clients/index.html', {'show_clients': False,'clients': clients})
+
+def show_clients(request, client_slug):
+    client = {
+        'name': 'Fred Lambert',
+        'phone': '+33 457894867',
+        'email': 'fredlamb02@gmail.com',
+        'location': 'Paris, France'
+    }
+    return render(request,'clients/client.html', {'client': client})
