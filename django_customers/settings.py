0cb5cd7390cf01e82ec7231e06acc49be6d001cc
honorezemagho <honorezemagho@gmail.com>
2021-07-15 05:55:02 +0100

    added postgresql db

--- django_customers/settings.py
+++ django_customers/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_customers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'clients',
+        'USER': 'honore237',
+        'PASSWORD': 'honore',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
