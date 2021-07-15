50c3d638f158022c52cd0f2cc91cffab05e3b8a2
honorezemagho <honorezemagho@gmail.com>
2021-07-15 06:20:49 +0100

    created some models

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
+ 
 # Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.IntegerField()
+    email = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    created_at = models.DateField(default=timezone.now)
+    updated_at = models.DateField(auto_now=True)
